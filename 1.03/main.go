5691cc59fb65038d04889a5d1560c8261908008d
Feyza Nur Şahin <141928475+Feyzanrs@users.noreply.github.com>
2024-05-15 23:31:53 +0300

    15 may

---
+++ 1.03/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	count := 8
+	var message string
+	if count > 5 {
+		message = "Greater than 5"
+	} else {
+		message = "Not greater than 5"
+	}
+	fmt.Println(message)
+}
---
+++ 2.01/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	input := 6
+	if input%2 == 0 {
+		fmt.Println(input, "is even")
+	}
+}
