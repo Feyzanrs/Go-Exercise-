03d7d2510774f6e6ab4df024c223a4d60994aebd
Feyza Nur Şahin <141928475+Feyzanrs@users.noreply.github.com>
2024-06-22 19:15:16 +0300

    Update to desktime data

--- desktime.userıdsiz/main.go
+++ desktime.userıdsiz/main.go
@@ -5,18 +5,25 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
 
-// DeskTimeData API'den dönen JSON yapısını modellemek için kullanılır
+// DeskTimeData models the JSON structure returned from the API
 type DeskTimeData struct {
-	DeskTimeTime int `json:"desktimeTime"`
+	DeskTimeTime int `json:"desktimeTime"` // The amount of time in seconds the employee has spent on desktime
+}
+
+// User struct to model the JSON structure for user API keys
+type User struct {
+	Name   string `json:"name"`
+	ApiKey string `json:"apiKey"`
 }
 
 func fetchDeskTimeData(apiKey string) {
-	// API URL'ini oluşturma
+	// Constructing the API URL
 	url := fmt.Sprintf("https://desktime.com/api/v2/json/employee?apiKey=%s", apiKey)
 
-	// HTTP GET isteği
+	// Making an HTTP GET request
 	resp, err := http.Get(url)
 	if err != nil {
 		fmt.Println("HTTP request failed:", err)
@@ -24,32 +31,54 @@ func fetchDeskTimeData(apiKey string) {
 	}
 	defer resp.Body.Close()
 
-	// Yanıtı okuma
+	// Reading the response body
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Println("Failed to read response body:", err)
 		return
 	}
 
-	// JSON yanıtını parse etme
+	// Parsing the JSON response
 	var data DeskTimeData
 	if err := json.Unmarshal(body, &data); err != nil {
 		fmt.Println("Failed to parse JSON response:", err)
 		return
 	}
 
-	// DeskTime süresini saat ve dakika olarak hesaplama
+	// Calculating the DeskTime in hours and minutes
 	hours := data.DeskTimeTime / 3600
 	minutes := (data.DeskTimeTime % 3600) / 60
 
-	// DeskTime değerini yazdırma
+	// Printing the DeskTime value
 	fmt.Printf("DeskTime for the employee: %d hours, %d minutes\n", hours, minutes)
 }
 
 func main() {
-	// API anahtarı
-	apiKey := "ca6c72efeafd79aca81022a6f00ada05"
+	// Opening JSON file with API keys
+	jsonFile, err := os.Open("users.json")
+	if err != nil {
+		fmt.Println("Error opening JSON file:", err)
+		return
+	}
+	defer jsonFile.Close()
 
-	// DeskTime verilerini getir
-	fetchDeskTimeData(apiKey)
+	// Reading the JSON file
+	jsonData, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		fmt.Println("Error reading JSON data:", err)
+		return
+	}
+
+	// Parsing the JSON data into User slice
+	var users []User
+	if err := json.Unmarshal(jsonData, &users); err != nil {
+		fmt.Println("Error parsing JSON data:", err)
+		return
+	}
+
+	// Iterating over users and fetching DeskTime data
+	for _, user := range users {
+		fmt.Printf("Fetching DeskTime data for %s\n", user.Name)
+		fetchDeskTimeData(user.ApiKey)
+	}
 }
